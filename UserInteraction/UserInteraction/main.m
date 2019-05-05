783219aa871612dfcad49e3e17e0c4fe36271deb
William James Oliver <w-j-oliver@hotmail.com>
2019-05-05 15:27:56 -0700

    comments on UI process

--- UserInteraction/UserInteraction/main.m
+++ UserInteraction/UserInteraction/main.m
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #include <stdio.h>
 
-
+//had a little fun with the example and went on a tangent for a bit, trying to accompany myself with this stuff more.
 int main()
 {
     char count[100];
@@ -19,7 +19,8 @@ int main()
     gets(count);
     printf("Enter a string to calculate it's length\n");
     scanf("%s", count);
-    amt = strlen(count);
+    //still confused on exactly what I did wrong here, the loop seems to halt when this happens, perhaps due to this warning?
+    amt = strlen(&count);
     printf("Length of the string = %d\n", amt);
 }
 
