903db0f31bfee1320541c8cb6532fff91831ae0c
William James Oliver <w-j-oliver@hotmail.com>
2019-05-05 15:25:08 -0700

    comments on fizzbuzz process

--- FizzBuzz/FizzBuzz/main.m
+++ FizzBuzz/FizzBuzz/main.m
@@ -12,11 +12,13 @@ int i;
 int x = 3;
 int n = 5;
 
+//this was the easiest one, as it is with things that are first I guess, I did struggle with...
 int main(){
     for(i = 1; i <= 100; i++){
         if(i % x == 0 && i % n == 0)
             printf("FizzBuzz \n");
         else if(i % x == 0)
+            //...checking if a variable is divisible by another but some searching around help, specifically used the example in LearnC.Org's While Loop tutorial
             printf("Fizz \n");
         else if(i % n == 0)
             printf("Buzz \n");
