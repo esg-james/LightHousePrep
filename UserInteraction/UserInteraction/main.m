0fd463732f385579be053cedabe3195f6cbd71ac
William James Oliver <w-j-oliver@hotmail.com>
2019-05-05 19:01:28 -0700

    Fixed the loop so it doesn't halt, but not sure if gets() is the wrong way to get the amount?

--- UserInteraction/UserInteraction/main.m
+++ UserInteraction/UserInteraction/main.m
@@ -14,13 +14,11 @@ int main()
 {
     char count[100];
     int amt;
-    
-    printf("Enter your name\n");
-    gets(count);
     printf("Enter a string to calculate it's length\n");
-    scanf("%s", count);
-    //still confused on exactly what I did wrong here, the loop seems to halt when this happens, perhaps due to this warning?
-    amt = strlen(&count);
+    scanf("%99s", count);
+    amt = strlen(count);
+    gets(count);
     printf("Length of the string = %d\n", amt);
+    //still confused on exactly what I did wrong here, the loop seems to halt when this happens, perhaps due to this warning?
 }
 
