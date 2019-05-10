4fec725911973f5d780cf6f12ebfc2b62f9029b7
William James Oliver <james@eastsidegamestudio.com>
2019-05-10 11:39:55 -0700

    module 5 calculator assignment

--- Calculate_Module5/Calculate_Module5/main.m
+++ Calculate_Module5/Calculate_Module5/main.m
@@ -6,16 +6,54 @@
 //  Copyright © 2019 James Oliver. All rights reserved.
 //
 
+//importing foundation library
 #import <Foundation/Foundation.h>
+
+
+//NSString *math = @"math";
+//function prototype
+void arithmetic(void);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        NSInteger firstNumber = 33;
-        NSInteger secondNumber = 66;
-        NSInteger result = firstNumber * secondNumber;
-        
-        NSLog(@"The first number is %i, the second number is %i, and the product of the two is %i",firstNumber,secondNumber,result);
+        //function call
+        arithmetic();
     }
     return 0;
+}
+
+void arithmetic(){
+    
+    //operations
+    enum operationType{add, subtract, multiply, divide};
+    enum operationType currentOperation = divide;
+    
+    //declarations
+    NSInteger firstNumber = 66;
+    NSInteger secondNumber = 33;
+    NSInteger result = firstNumber / secondNumber;
+    //NSString *math = @"addition";
+    
+    
+    switch(currentOperation){
+        case add:
+            result = firstNumber + secondNumber;
+            NSLog(@"the operation is addition and the result is %li",result);
+            break;
+        case subtract:
+            result = firstNumber - secondNumber;
+            NSLog(@"the operation is subtraction and the result is %li",result);
+            break;
+        case multiply:
+            result = firstNumber * secondNumber;
+            NSLog(@"the operation is multiplication and the result is %li",result);
+            break;
+        case divide:
+            result = firstNumber / secondNumber;
+            NSLog(@"the operation is division and the result is %li",result);
+            break;
+    }
+    //NSLog(math,currentOperation,result);
 }
