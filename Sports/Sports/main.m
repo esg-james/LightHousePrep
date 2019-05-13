ceec1bf561d79a2dbdef144e04e2ac2418750e32
William James Oliver <james@eastsidegamestudio.com>
2019-05-13 14:55:14 -0700

    assigment for mutable arrays in module 5, 2nd step

--- Sports/Sports/main.m
+++ Sports/Sports/main.m
@@ -10,13 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
-        NSArray *sports = @[@"Hockey",
+        
+        
+        
+        NSMutableArray *sports = [[NSMutableArray alloc]initWithObjects:@"Hockey",
+                                  @"Wakeboarding",
+                                  @"Wakesurfing",
+                                  @"Slalom Skiing", nil];
+        NSLog(@"The 1st sport is %@",[sports objectAtIndex:0]);
+        [sports removeLastObject];
+        [sports insertObject:@"Slalom Skiing" atIndex:0];
+        NSLog(@"The 1st sport is now %@",[sports objectAtIndex:0]);
+        NSLog(@"%@ is now the  second sport",[sports objectAtIndex:1]);
+        /*NSMutableArray *sports = @[@"Hockey",
                             @"Wakeboarding",
                             @"Wakesurfing",
-                            @"Slalom Skiing"];
+                            @"Slalom Skiing"];*/
+        for(NSArray *n in sports)
+            NSLog(@"%@",n);
+            
         
-        NSLog(@"The second sport is %@",[sports objectAtIndex:1]);
         
         
     }
