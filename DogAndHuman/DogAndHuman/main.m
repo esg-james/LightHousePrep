a4fdb409c613960ca5f0ca02dc57d39690d836ad
William James Oliver <james@eastsidegamestudio.com>
2019-05-14 22:50:51 -0700

    assigned person as dogs owner and printed it, other two pet calls are null?

--- DogAndHuman/DogAndHuman/main.m
+++ DogAndHuman/DogAndHuman/main.m
@@ -20,18 +20,23 @@ int main(int argc, const char * argv[]) {
         Dog *betty2 = [[Dog alloc]init];
         [betty setSpeak:@"Woof"];
         //Person *myPet = [Dog set]
-        [betty setName:@"Black Betty"];
-        james.pet = betty;
-        NSLog(@"%@",james.pet.name);
         james.pet = betty1;
         NSLog(@"%@",james.pet.name);
         james.pet = betty2;
         NSLog(@"%@",james.pet.name);
+        [betty setName:@"Black Betty"];
+        james.pet = betty;
+        NSLog(@"%@",james.pet.name);
+        
         
         [james setName:@"Jum"];
         
         NSLog(@"my name is now %@",james.name);
+        NSLog(@"My dog is %@",james.pet.name);
         NSLog(@"%@",[betty speak]);
+        
+        //synthesizers automatically create get and set methods for those definitions to be used outside of the class
+        //can use object.calls to assign values to other objects ie. my dog is black betty
         
         
     }
