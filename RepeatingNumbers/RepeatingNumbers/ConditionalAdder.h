b4d2ae7200253854a7a2619c20c5867e06a70327
William James Oliver <james@eastsidegamestudio.com>
2019-05-19 14:54:41 -0700

    Finished Kata 2, starting to get the hang of this, need to think 1 step at a time and log everything - REMEMBER to convert variables appropriately when conversion is failing implicitly

---
+++ RepeatingNumbers/RepeatingNumbers/ConditionalAdder.h
@@ -0,0 +1,23 @@
+//
+//  ConditionalAdder.h
+//  RepeatingNumbers
+//
+//  Created by James Oliver on 2019-05-19.
+//  Copyright © 2019 James Oliver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConditionalAdder : NSObject
+
+@property NSArray* theArray;
+
+- (instancetype)initWithNumbers:(NSArray *)numbers;
+
+- (int)sumWithCondition:(NSString *)condition;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- RepeatingNumbers/RepeatingNumbers/main.m
+++ RepeatingNumbers/RepeatingNumbers/main.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "NumberRepeater.h"
+#import "ConditionalAdder.h"
 
 
 int main(int argc, const char * argv[]) {
@@ -27,12 +28,13 @@ int main(int argc, const char * argv[]) {
         NumberRepeater *repeater3 = [[NumberRepeater alloc] initWithNumbers: @[@[@10, @4], @[@34, @6], @[@92, @2]]];
         NSLog(@"%@", [repeater3 process]);
         
-       /* NSMutableString* output = [NSMutableString stringWithFormat:@"response: %@",[JSON valueForKeyPath:@"Status"]];
-        int x;
-        for (x=0; x<[dataarray count]; x++) {
-            NSLog(@"%d : %@",x, [dataarray objectAtIndex:x]);
-            [output appendFormat:@" %@ ",[dataarray objectAtIndex:x]];
-        }*/
+        ConditionalAdder *adder1 = [[ConditionalAdder alloc] initWithNumbers:@[@1, @2, @3, @4, @5]];
+        NSLog(@"%i", [adder1 sumWithCondition:@"even"]);
+        NSLog(@"%i", [adder1 sumWithCondition:@"odd"]);
+        ConditionalAdder *adder2 = [[ConditionalAdder alloc] initWithNumbers:@[@13, @88, @12, @44, @99]];
+        NSLog(@"%i", [adder2 sumWithCondition:@"even"]);
+        ConditionalAdder *adder3 = [[ConditionalAdder alloc] initWithNumbers:@[]];
+        NSLog(@"%i", [adder3 sumWithCondition:@"odd"]);
         
         
         
