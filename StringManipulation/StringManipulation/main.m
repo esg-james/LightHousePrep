43848857a93af1498f3f2812671288fce54290c7
William James Oliver <james@eastsidegamestudio.com>
2019-05-27 16:48:38 -0700

    Finished assignment for the day

--- StringManipulation/StringManipulation/main.m
+++ StringManipulation/StringManipulation/main.m
@@ -18,38 +18,38 @@ int main(int argc, const char * argv[]) {
             int number;
             char inputChars[255];
             
-            
-            
             printf("Select an operation: ");
             scanf("%d",&number);
+            
             printf("Input a string: ");
             scanf("%c",inputChars);
+            
             fgets(inputChars, 255, stdin);
             strtok(inputChars, "\n");
             NSString *inputString = [NSString stringWithUTF8String:inputChars];
-            ManipulateDatString * maker1 = [[ManipulateDatString alloc]initWithString:inputString];
+            ManipulateDatString *maker1 = [[ManipulateDatString alloc]initWithString:inputString];
             
             switch (number) {
                 case 1:
-                    NSLog(@"%@",[maker1 upCase]);
+                    NSLog(@"The string is now: %@ and it is located at Mem. Address: %p",[maker1 upCase], &maker1);
                     break;
                 case 2:
-                    NSLog(@"%@", [maker1 downCase]);
+                    NSLog(@"The string is now: %@ and it is located at Mem. Address: %p",[maker1 downCase], &maker1);
                     break;
                 case 3:
-                    NSLog(@"%ld", (long)[maker1 stringToInt]);
+                    NSLog(@"The length of this string is: %ld and it is located at Mem. Address %p", (long)[maker1 stringToInt], maker1);
                     break;
                 case 4:
-                    NSLog(@"%@", [maker1 canadianEh]);
+                    NSLog(@"The string is really Canadian now: %@ and it is located at Mem. Address: %p",[maker1 canadianEh], &maker1);
                     break;
                 case 5:
-                    NSLog(@"%@", [maker1 questionableExclamation]);
+                    NSLog(@"The string is now a teenager in puberty: %@ and it is located at Mem. Address: %p",[maker1 questionableExclamation], &maker1);
                     break;
                 case 6:
-                    NSLog(@"%@", [maker1 deSpace]);
+                    NSLog(@"The string now has a bunch of dumb hiphens in it: %@ and it is located at Mem. Address: %p",[maker1 deSpace], &maker1);
                     break;
                 case 7:
-                    NSLog(@"%@", [maker1 replaceMyString]);
+                    NSLog(@"The old switcheroo: %@ is located at Mem. Address: %p",[maker1 replaceMyString], &maker1);
                     break;
                     
                 default:
