285ce29fcb745be0abdecd564b8e9e179e6e7db4
William James Oliver <james@eastsidegamestudio.com>
2019-06-04 14:22:19 -0700

    Autolayout Storyboard start

---
+++ AutoLayout_Storyboard/AutoLayout_Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout_Storyboard
+//
+//  Created by James Oliver on 2019-06-04.
+//  Copyright © 2019 James Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayout_Storyboard/AutoLayout_Storyboard/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AutoLayout_Storyboard
+//
+//  Created by James Oliver on 2019-06-04.
+//  Copyright © 2019 James Oliver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
