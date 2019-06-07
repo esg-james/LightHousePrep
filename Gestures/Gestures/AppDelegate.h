2fc211356e888e26fabc7762d9aea9c5f4832d0c
William James Oliver <james@eastsidegamestudio.com>
2019-06-06 17:39:37 -0700

    First commit: Gestures

---
+++ Gestures/Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by James Oliver on 2019-06-06.
+//  Copyright © 2019 James Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
